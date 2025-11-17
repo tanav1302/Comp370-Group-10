48a370002482d7c816aa629c96625749adde6996
tanav1302 <bansaltanav2004@gmail.com>
2025-11-17 16:27:00 -0500

    data_collection.py

--- script/data_collection.py
+++ script/data_collection.py
@@ -13,21 +13,29 @@ def main():
     with open(os.path.join(data_path,'mlp_s1-8.csv'), 'r') as input_file:
         df = pd.read_csv(input_file)
         df_character = df[df['pony'] == args.character_name]
-        # remove title column
-        df_character_title = df_character.drop(columns=['title'])
-        # remvove writer column
-        df_character_title_writer = df_character_title.drop(columns=['writer'])
-        # ramdom 350 rows
-        df_character_title_writer_350 = df_character_title_writer.sample(n=350, random_state=1)
+
+        # get number of unique titles
+        unique_titles = df_character['title'].nunique() 
+        # list of unique titles
+        unique_titles_list = df_character['title'].unique().tolist()
+        
+
+        lines_per_title = 350 / unique_titles
+        df_character_350= pd.DataFrame()
+        
+
+        for title in unique_titles_list:
+            df_title = df_character[df_character['title'] == title]
+            # get top lines_per_title rows
+            df_title_top = df_title.head(min(int(lines_per_title),len(df_title)))
+            
+            df_character_350 = pd.concat([df_character_350, df_title_top], ignore_index=True)   
+
+
+        df_character_350 = df_character_350.drop(columns=['writer'])
 
         with open(os.path.join(data_path,f'{args.character_name}_dialogue.csv'), 'w') as output_file:
-            df_character_title_writer_350.to_csv(output_file, index=False)
-
-
-
-
-
-
+            df_character_350.to_csv(output_file, index=False)
 
 
 
